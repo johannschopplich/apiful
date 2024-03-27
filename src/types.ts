5bbe9068bf7854972ddee6ff9db57e196eb7329e
Johann Schopplich <mail@johannschopplich.com>
2024-03-27 07:13:39 +0100

    perf: import response types from `ofetch`

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,1 @@
 export interface OpenAPISchemaRepository {}
-
-// TODO: Import from ofetch after next release
-export interface ResponseMap {
-  blob: Blob
-  text: string
-  arrayBuffer: ArrayBuffer
-}
-
-export type ResponseType = keyof ResponseMap | 'json'
-export type MappedResponseType<
-  R extends ResponseType,
-  JsonType = any,
-> = R extends keyof ResponseMap ? ResponseMap[R] : JsonType
