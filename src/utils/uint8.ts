f051d0c56f1e8c2f2d0bdeccfb25883d343a99fd
Johann Schopplich <mail@johannschopplich.com>
2024-11-09 09:22:41 +0100

    refactor: use `charCodeAt`

--- src/utils/uint8.ts
+++ src/utils/uint8.ts
@@ -1,14 +1,14 @@
 export function base64ToUint8Array(base64String: string) {
   const base64Url = base64String.replaceAll('-', '+').replaceAll('_', '/')
   const latin1String = globalThis.atob(base64Url)
-  return Uint8Array.from(latin1String, byte => byte.codePointAt(0)!)
+  return Uint8Array.from(latin1String, byte => byte.charCodeAt(0))
 }
 
 export function uint8ArrayToBase64(array: Uint8Array) {
   let latin1String = ''
 
   for (const byte of array) {
-    latin1String += String.fromCodePoint(byte)
+    latin1String += String.fromCharCode(byte)
   }
 
   return globalThis.btoa(latin1String)
