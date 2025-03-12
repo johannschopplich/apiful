3cc48c46dba022586b79d0d9f9aa8f20458743e0
Johann Schopplich <mail@johannschopplich.com>
2025-03-12 19:21:33 +0100

    test: rename Echo API schema file

--- playground/apiful.config.ts
+++ playground/apiful.config.ts
@@ -3,7 +3,7 @@ import { defineApifulConfig } from '../src/config'
 export default defineApifulConfig({
   services: {
     testEcho: {
-      schema: '../test/fixtures/test-echi-api-schema.yml',
+      schema: '../test/fixtures/test-echo-api-schema.yml',
     },
     petStore: {
       schema: 'schemas/pet-store.json',
