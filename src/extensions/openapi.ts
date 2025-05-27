6943a268880405207fa7b4e1d14986824ba09ec5
Johann Schopplich <mail@johannschopplich.com>
2025-05-27 06:55:46 +0200

    feat: rename and export `SchemaPaths` type (closes #7)

--- src/extensions/openapi.ts
+++ src/extensions/openapi.ts
@@ -4,15 +4,15 @@ import type { OpenAPIClient } from '../openapi/types.ts'
 import { ofetch } from 'ofetch'
 import { resolvePathParams } from '../openapi/index.ts'
 
-type ExtractPaths<K> = K extends keyof OpenAPISchemaRepository
+export type * from '../openapi/types.ts'
+
+export type SchemaPaths<K> = K extends keyof OpenAPISchemaRepository
   ? OpenAPISchemaRepository[K]
   : Record<string, never>
 
-export type * from '../openapi/types.ts'
-
 export function OpenAPIBuilder<
   const Schema extends string,
-  Paths = ExtractPaths<Schema>,
+  Paths = SchemaPaths<Schema>,
 >() {
   return function (client: ApiClient): OpenAPIClient<Paths> {
     const fetcher = ofetch.create(client.defaultOptions)
