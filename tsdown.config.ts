faeed8f6ed7dffd4644788773f8d8753727f54e2
Johann Schopplich <mail@johannschopplich.com>
2025-06-13 15:55:43 +0200

    chore: dynamically infer entry exports from package.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,17 +1,12 @@
 import type { UserConfig, UserConfigFn } from 'tsdown/config'
 import { defineConfig } from 'tsdown/config'
 
+const { exports } = await import('./package.json', { with: { type: 'json' } })
+
 const config: UserConfig | UserConfigFn = defineConfig({
   entry: [
+    ...([...inferExports(exports)].filter(entry => !entry.endsWith('schema.ts'))),
     'src/cli/index.ts',
-    'src/config.ts',
-    'src/extensions/index.ts',
-    'src/index.ts',
-    'src/openapi/index.ts',
-    'src/openapi/client.ts',
-    'src/generated/http-status-codes.ts',
-    'src/generated/http-status-phrases.ts',
-    'src/utils/index.ts',
   ],
   external: [
     'apiful/schema',
@@ -23,3 +18,22 @@ const config: UserConfig | UserConfigFn = defineConfig({
 })
 
 export default config
+
+function inferExports(exports: Record<string, unknown>): Set<string> {
+  const entries = new Set<string>()
+
+  for (const value of Object.values(exports)) {
+    if (typeof value === 'string') {
+      if (value.endsWith('.js')) {
+        entries.add(value.replace('./dist', './src').replace('.js', '.ts'))
+      }
+    }
+    else if (typeof value === 'object' && value !== null) {
+      for (const entry of inferExports(value as Record<string, unknown>)) {
+        entries.add(entry)
+      }
+    }
+  }
+
+  return entries
+}
