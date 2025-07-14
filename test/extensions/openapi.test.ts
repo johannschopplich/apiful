2d00150a3cad9e745ecbbc955029dbfcfc77ad8e
Johann Schopplich <mail@johannschopplich.com>
2025-07-14 16:36:44 +0200

    chore: simplify imports

--- test/extensions/openapi.test.ts
+++ test/extensions/openapi.test.ts
@@ -1,5 +1,4 @@
-import type { OpenAPISchemaRepository } from 'apiful/schema'
-import type { PetStore } from 'apiful/schema'
+import type { OpenAPISchemaRepository, PetStore } from 'apiful/schema'
 import type { components } from 'apiful/schema/petStore'
 import type { ApiClient } from '../../src/client'
 import type { OpenAPIClient } from '../../src/extensions/openapi'
