a1dc88b82b6e078d3b716cab935503ea0abf0fb4
Johann Schopplich <mail@johannschopplich.com>
2025-12-17 15:48:15 +0100

    refactor: function signatures and variable names

--- src/utils/uint8.ts
+++ src/utils/uint8.ts
@@ -1,15 +1,15 @@
-export function base64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
-  const base64Url = base64String.replaceAll('-', '+').replaceAll('_', '/')
-  const latin1String = globalThis.atob(base64Url)
-  return Uint8Array.from(latin1String, byte => byte.charCodeAt(0))
+export function base64ToUint8Array(base64String: string): Uint8Array {
+  const normalizedBase64 = base64String.replaceAll('-', '+').replaceAll('_', '/')
+  const binaryString = globalThis.atob(normalizedBase64)
+  return Uint8Array.from(binaryString, char => char.charCodeAt(0))
 }
 
 export function uint8ArrayToBase64(array: Uint8Array): string {
-  let latin1String = ''
+  let binaryString = ''
 
   for (const byte of array) {
-    latin1String += String.fromCharCode(byte)
+    binaryString += String.fromCharCode(byte)
   }
 
-  return globalThis.btoa(latin1String)
+  return globalThis.btoa(binaryString)
 }
